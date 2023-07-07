d70f5ce971e37b93a908e9446c03659fbdc1fd05
Sharminaktermetu <sharminacc1@gmail.com>
2023-07-07 23:13:52 +0600

    first commit

---
+++ src/Routes/router.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, } from "react-router-dom";
+import Main from "../components/Main/Main";
+import Home from "../components/Home/Home";
+import Details from "../components/Details/Details";
+import Login from "../components/Login/Login";
+import Signup from "../components/Signup/Signup";
+import PrivateRoute from "./PrivateRoute";
+import Error from "../components/ErrorPage/Error";
+import Blogs from "../components/Blogs/Blogs";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement:<Error></Error>,
+    children: [{
+      path: "/",
+      element: <Home></Home>
+    },
+
+    {
+      path: "/data/:id",
+      element:<PrivateRoute> <Details></Details></PrivateRoute>,
+      loader:({params})=>fetch(`https://server-chef-adsmarketersaif13-gmailcom.vercel.app/data/\
+      ${params.id}`)
+    },
+    {
+      path: "/login",
+      element: <Login></Login>,
+     
+    },
+    {
+      path: "/signup",
+      element: <Signup></Signup>,
+     
+    },
+    {
+      path: "/blogs",
+      element: <Blogs></Blogs>,
+     
+    },
+
+
+    ]
+  },
+
+]);
+
+export default router;
